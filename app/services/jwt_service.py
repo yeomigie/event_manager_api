3781b1c4e149eb6933061e34ad2abec178d3a52f
yeomigie <yeo@njit.edu>
2025-04-24 03:00:04 +0500

    Fixing error

--- app/services/jwt_service.py
+++ app/services/jwt_service.py
@@ -14,6 +14,16 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
     return encoded_jwt
 
+def create_access_token(*, data: dict, expires_delta: timedelta = None):
+     to_encode = data.copy()
+     # Convert role to string and then to uppercase before encoding the JWT
+     if 'role' in to_encode:
+         to_encode['role'] = str(to_encode['role']).upper()  # Convert role to string first
+     expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=settings.access_token_expire_minutes))
+     to_encode.update({"exp": expire})
+     encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
+     return encoded_jwt
+
 def decode_token(token: str):
     try:
         decoded = jwt.decode(token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm])
